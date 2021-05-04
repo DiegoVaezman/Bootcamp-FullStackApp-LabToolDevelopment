4e9ab8347a4fcc3a26ebc2f173c42eb0646c8625
DiegoVaezman <diego.ng90@gmail.com>
2021-05-04 13:09:12 +0200

    fixing refresh

--- middlewares/protectedRoute.js
+++ middlewares/protectedRoute.js
@@ -4,19 +4,21 @@ const tokenpass = process.env.TOKENPASS
 
 //middleware token authentication
 const protectedRoute = (req, res, next) => {
-   
+    console.log("protected rute")
     let token = req.headers.authorization;
-  
+  console.log(token)
     if (token) {
         token = token.replace('Bearer ', '')
         jwt.verify(token, tokenpass, (err, decoded) => {      
         if (err) {
+          console.log("error verificando token invalid token")
           return res.status(401).send({ msg: 'Invalid token' });    
         }
         req.decoded = decoded;  
         next();
       });
     } else {
+      console.log("no hay token")
     res.status(401).send({ msg: 'Token no provided.'});
     }
  };
